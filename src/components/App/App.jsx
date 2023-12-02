b8e09300e646e96cb0b0205d268aa3840327a718
MDevelope <mousa.developer22@gmail.com>
2023-12-02 14:53:04 +0200

    make animation to x / o buttons

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,8 +1,11 @@
-import './App.css'
-import Title from '../Widget/Title/Title.jsx'
-import Card from '../Widget/Card/Card.jsx'
-import Button from '../Widget/Button/Button.jsx'
+import './App.css';
+import Title from '../Widget/Title/Title.jsx';
+import Card from '../Widget/Card/Card.jsx';
+import Button from '../Widget/Button/Button.jsx';
+import { useState } from 'react';
 function App() {
+  const [lastClicked, setLastClicked] = useState('');
+
   return (
     <div className="App">
       <div className="row">
@@ -12,10 +15,18 @@ function App() {
       <Card className="card">
         <Title children="player-btn" className="titleCard" />
         <div className="choose-btn">
-          <Button className="x-btn" children="X" />
-          <Button className="o-btn" children="O" />
-        </div>
-        <Title children="REMEMBER : X GOES FIRST" className="subTitle" />  
+          <Button 
+              className={`x-btn ${lastClicked === 'X' ? 'active' : 'not-active'}`} 
+              children="X" 
+              onClick={() => setLastClicked('X')} 
+            />
+            <Button 
+              className={`o-btn ${lastClicked === 'O' ? 'active' : 'not-active'}`} 
+              children="O" 
+              onClick={() => setLastClicked('O')} 
+            />
+          </div>
+          <Title children="REMEMBER : X GOES FIRST" className="subTitle" />  
       </Card>
       <Button className="cpu-btn" children="NEW GAME (VS CPU)" />
       <Button className="player-btn" children="NEW GAME (VS PLAYER)" />
